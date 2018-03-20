a78dde427ffbe9ae3a04c3ba97082a7b475748f1
muzmov <leshiffre@mail.ru>
2018-03-20 14:07:23 +0300

    Task 4

---
+++ Task4/index.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+
+function read(dir, done) {
+    var results = {
+        files: [],
+        dirs: []
+    };
+    fs.readdir(dir, function (err, list) {
+        if (err) return done(err);
+        var pending = list.length;
+        if (!pending) return done(null, results);
+        list.forEach(function (file) {
+            file = path.resolve(dir, file);
+            fs.stat(file, function (err, stat) {
+                if (stat && stat.isDirectory()) {
+                    results.dirs.push(file);
+                    read(file, function (err, res) {
+                        results.files = results.files.concat(res.files);
+                        results.dirs = results.dirs.concat(res.dirs);
+                        if (!--pending) done(null, results);
+                    });
+                } else {
+                    results.files.push(file);
+                    if (!--pending) done(null, results);
+                }
+            });
+        });
+    });
+}
+
+const dir = process.argv[2] || 'node_modules';
+
+read(dir, (err, res) => {
+    console.log("files:");
+    res.files.forEach(f => console.log(f));
+    console.log("dirs:");
+    res.dirs.forEach(d => console.log(d));
+});
